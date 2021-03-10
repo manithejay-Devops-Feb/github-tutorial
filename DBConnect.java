dd63047c743205bf7f0f8d15e7dd4ce77621df36
manitheja420 <ymanitheja@gmail.com>
2021-03-10 09:30:38 +0530

    second commit DBConnect and Users.java

--- DBConnect.java
+++ DBConnect.java
@@ -1,4 +1,3 @@
 Hello DBConnect.java
 git hub is very important....
-so dont neglect git hub technology 
-practice daily to get good grip in Git 
+so dont neglect git hub technology,practice daily to get good grip in Git 
