02f2d323ab1698912630281b4dec4a0784f98b5b
manitheja420 <ymanitheja@gmail.com>
2021-03-10 17:13:32 +0530

    updated-2 file DBC

--- DBConnect.java
+++ DBConnect.java
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 Reverted file
+=======
+>>>>>>> parent of 6c7f787 (updated DBConnect file)
 Hello DBConnect.java
 git hub is very important....
 so dont neglect git hub technology,practice daily to get good grip in Git 
